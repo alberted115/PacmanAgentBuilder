3a7e5466ed4505c2c4fbcdd68d388890ac0d8f66
SpicyOverlord <spicyoverlord@gmail.com>
2024-03-01 12:52:46 +0100

    Update runner.py

--- PacmanAgentBuilder/runner.py
+++ PacmanAgentBuilder/runner.py
@@ -3,4 +3,4 @@ from PacmanAgentBuilder.utils.runnerFunctions import *
 
 # runGameWithHuman()
 stats = calculatePerformanceOverXGames(MyFirstAgent, gameCount=10, gameSpeed=1, startLevel=0,
-                                       ghostsEnabled=False, freightEnabled=True, logging=True)
+                                       ghostsEnabled=True, freightEnabled=True, logging=True)
