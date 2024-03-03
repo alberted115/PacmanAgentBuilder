26bf252deba1cd4e98a02f6fc8311d9f552a4171
SpicyOverlord <spicyoverlord@gmail.com>
2024-03-03 09:47:04 +0100

    reformat argument for better readability

--- runner.py
+++ runner.py
@@ -2,6 +2,13 @@ from PacmanAgentBuilder.agents.MyFirstAgent import MyFirstAgent
 from PacmanAgentBuilder.utils.runnerFunctions import *
 
 # runGameWithHuman()
-stats = calculatePerformanceOverXGames(agentClass=MyFirstAgent, gameCount=10, gameSpeed=1, startLevel=0,
-                                       ghostsEnabled=True, freightEnabled=True,
-                                       lockDeltaTime=True, logging=True)
+stats = calculatePerformanceOverXGames(
+    agentClass=MyFirstAgent,
+    gameCount=10,
+    gameSpeed=1,
+    startLevel=0,
+    ghostsEnabled=True,
+    freightEnabled=True,
+    lockDeltaTime=True,
+    logging=True
+)
