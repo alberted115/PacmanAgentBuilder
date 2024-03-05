d4db6987a94e22103b24e0ba25ed877f0a38ee1a
SpicyOverlord <spicyoverlord@gmail.com>
2024-03-05 16:56:01 +0100

    lowered delta time when it is locked

--- Pacman_Complete/run.py
+++ Pacman_Complete/run.py
@@ -94,7 +94,7 @@ class GameController(object):
 
     def update(self):
         if self.lockDeltaTime:
-            dt = 0.04
+            dt = 0.034
         else:
             dt = self.clock.tick(30 * self.gameSpeed) / (1000.0 / self.gameSpeed)
 
